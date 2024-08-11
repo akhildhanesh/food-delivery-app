4317a3f3a46a91e0e2b9e9de1e460410289325f3
Akhil Dhanesh <akhildhanesh@icloud.com>
2024-08-11 19:57:36 +0530

    implement cart items icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home')
+  const { getTotalCartAmount } = useContext(StoreContext)
   return (
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
@@ -18,7 +20,9 @@ const Navbar = ({ setShowLogin }) => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-          <div className="dot"></div>
+          {
+            getTotalCartAmount() > 0 && <div className="dot"></div>
+          }   
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
