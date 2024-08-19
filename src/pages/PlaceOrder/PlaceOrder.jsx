0f529d4c28c154a31cab51d231c8fb59deeb49c3
Akhil Dhanesh <akhildhanesh@icloud.com>
2024-08-19 18:41:40 +0530

    implement place-order page

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext)
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className='place-order-left'>
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' name="" id="" />
+          <input type="text" placeholder='Last Name' name="" id="" />
+        </div>
+        <input type="email" placeholder='Email Address' name="" id="" />
+        <input type="text" placeholder='Street' name="" id="" />
+        <div className="multi-fields">
+          <input type="text" placeholder='City' name="" id="" />
+          <input type="text" placeholder='State' name="" id="" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip Code' name="" id="" />
+          <input type="text" placeholder='Country' name="" id="" />
+        </div>
+        <input type="text" placeholder='Phone' name="" id="" />
+      </div>
+      <div className='place-order-right'>
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className='cart-total-details'>
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <b>Total</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>Proceed to Payment</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
